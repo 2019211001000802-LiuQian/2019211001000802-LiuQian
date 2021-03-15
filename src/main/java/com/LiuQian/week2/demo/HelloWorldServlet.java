86b708a42f729ffcd871b2034c20ef4a34cfb83b
刘千 <1160537386@qq.com>
2021-03-15 20:50:25 +0800

    HW-week2-commit

---
+++ src/main/java/com/LiuQian/week2/demo/HelloWorldServlet.java
@@ -0,0 +1,25 @@
+package com.LiuQian.week2.demo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class HelloWorldServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("Name:LiuQian");
+        writer.println("ID:2019211001000802");
+        Date date = new Date();
+        writer.println("Date and Time" + date.toString());
+    }
+        public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+            PrintWriter writer = response.getWriter();
+            writer.println("Hello Client");
+        }
+    }
+
+
